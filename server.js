f29c507c7ee561436f2e4eb1085e4a00bca7416c
yagmurcetintas <yagmurcetin@gmail.com>
2019-08-06 11:59:36 -0400

    added server.js instructions for using local mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express= require ("express");
+//require express and mongoose
+const express = require ("express");
 const app = express();
+const mongoose = require("mongoose");
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/bootcampspot_db";
+mongoose.connect(MONGODB_URI);
 
 app.get("/", (req, res) => res.send("API running!"));
 
 const PORT = process.env.PORT || 5000;
+
+// start server
 app.listen(PORT, () => console.log(`Backend server started on port: ${PORT}`));
