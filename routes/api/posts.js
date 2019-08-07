3251ab24e614cfe6d2ef6354cb7596f401fa296c
yagmurcetintas <yagmurcetin@gmail.com>
2019-08-07 15:12:09 -0400

    added get routes for getting all posts and getting posts by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -39,8 +39,42 @@ router.post('/', [auth,
         console.error(err.message);
         res.status(500).send("Server Error");
     }
+});
 
+// @route  GET api/posts
+// @desc   Get all posts
+// @access private
 
+router.get("/", auth, async (req, res) => {
+    try {
+        //get posts, sort them by date, the newest one should be at the top
+        const posts = await Post.find().sort({ date: -1 });
+        return res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
+
+// @route  GET api/posts/:id
+// @desc   Get posts by id
+// @access private
+
+router.get("/:id", auth, async (req, res) => {
+    try {
+        //get posts, sort them by date, the newest one should be at the top
+        const post = await Post.findById(req.params.id);
+        if(!post) {
+            return res.status(404).json({ msg: "Post not found"});  
+        }
+        return res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === "ObjectId") {
+            return res.status(404).json({ msg: "Post not found"});  
+        }
+        res.status(500).send("Server error");
+    }
 });
 
 module.exports = router;
