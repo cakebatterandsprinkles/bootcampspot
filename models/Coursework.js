1142190ab58aa62c348f57838c44dc3564c7857c
yagmurcetintas <yagmurcetin@gmail.com>
2019-08-07 22:26:43 -0400

    added Coursework schema

---
+++ models/Coursework.js
@@ -0,0 +1,38 @@
+const mongoose = require ("mongoose");
+const CourseworkSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    submissions: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "user"
+            },
+            submissionlinks: [
+                {
+                    link: {
+                        type: String,
+                    }
+                }
+            ],
+            grade: {
+                type: String,
+            },
+            feedback: {
+                type: String,
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Coursework = mongoose.model("coursework", CourseworkSchema);
