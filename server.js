1ec14c954665020b568d22aa62cc46d0c1ec281e
yagmurcetintas <yagmurcetin@gmail.com>
2019-08-13 23:48:31 -0400

    Changed routing to react

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.use ("/api/coursework", require("./routes/api/coursework"));
 app.use ("/api/sessions", require("./routes/api/sessions"));
 app.use ("/api/support", require("./routes/api/support"));
 
-app.use(function (req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-})
+});
 
 // start server
 app.listen(PORT, () => console.log(`Backend server started on port: ${PORT}`));
